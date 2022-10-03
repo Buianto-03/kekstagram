f5d4f383a0c2c98d9018f8322e7cf6d6d0c1f4d7
Buianto-03 <buyanto678@gmail.com>
2022-10-03 12:46:24 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// let string
+// const commentLength = function stringMaxLength (string) {
+//     const MAXLENGTH = 140;
+//     if (string.length < MAXLENGTH) {
+//         console.log('длина строки: ' + string.length);
+//     } else {alert('Комментарий не должен содержать более 140 символов')}
+// }
+
+// commentLength('Классная фотка!');
+// console.log('564');
+
+// let upload = document.querySelector('.upload-file')
+// console.log(upload);
+
+console.log(document.querySelector('.upload-file'))
